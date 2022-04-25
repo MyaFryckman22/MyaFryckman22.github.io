64906f7e067bec6727ab9485924731f3317eca97
MyaFryckman22 <817258@mystma.org>
2022-04-25 11:35:43 -0500

    STMA #1 page initial

---
+++ stma.js
@@ -0,0 +1,35 @@
+function activitypage() {
+	window.open("https://stmaknights.com/");
+}
+function eventpage() {
+	window.open("https://www.stma.k12.mn.us/Page/2#calendar1/20220422/month");
+}
+function yearpage() {
+	window.open("https://www.stma.k12.mn.us/domain/4555");
+}
+function pvpage() {
+	window.open("https://pvue7.region1.k12.mn.us/rt0885/PXP2_Login.aspx");
+}
+function menupage() {
+	window.open("https://stma.nutrislice.com/menus-eula");
+}
+function jobspage() {
+	window.open("https://www.stma.k12.mn.us/domain/2433");
+}
+function swpage() {
+	window.open("https://pac.stma.k12.mn.us/scripts/wsisa.dll/WService=wsFin/seplog01.w");
+}
+function emailpage() {
+	window.open("https://mail.google.com/mail/u/0/?pli=1#inbox");
+}
+
+
+
+document.getElementById("activity").addEventListener("click", activitypage);
+document.getElementById("event").addEventListener("click", eventpage);
+document.getElementById("year").addEventListener("click", yearpage);
+document.getElementById("PV").addEventListener("click", pvpage);
+document.getElementById("menu").addEventListener("click", menupage);
+document.getElementById("jobs").addEventListener("click", jobspage);
+document.getElementById("sw").addEventListener("click", swpage);
+document.getElementById("email").addEventListener("click", emailpage);
