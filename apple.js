647a0ccc8b586619b2b238fabddeae8600320755
MyaFryckman22 <817258@mystma.org>
2022-04-28 12:09:47 -0500

    Apple website remake

---
+++ apple.js
@@ -0,0 +1,61 @@
+function thirteenpro() {
+	window.open("https://www.apple.com/iphone-13-pro/");
+}
+function thirteen() {
+	window.open("https://www.apple.com/iphone-13/");
+}
+function se() {
+	window.open("https://www.apple.com/iphone-se/");
+}
+function twelve() {
+	window.open("https://www.apple.com/iphone-12/key-features/");
+}
+function eleven() {
+	window.open("https://www.apple.com/shop/buy-iphone/iphone-11");
+}
+function compare() {
+	window.open("https://www.apple.com/iphone/compare/");
+}
+function airpods() {
+	window.open("https://www.apple.com/airpods/");
+}
+function airtag() {
+	window.open("https://www.apple.com/airtag/");
+}
+function accessories() {
+	window.open("https://www.apple.com/shop/iphone/accessories");
+}
+function applecard() {
+	window.open("https://www.apple.com/apple-card/");
+}
+function ios() {
+	window.open("https://www.apple.com/ios/ios-15/");
+}
+function shop() {
+	window.open("https://www.apple.com/shop/buy-iphone");
+}
+function buy() {
+	window.open("https://www.apple.com/shop/buy-iphone/iphone-13-pro");
+}
+function link() {
+	window.open("https://www.apple.com/apple-card/monthly-installments/");
+}
+function more() {
+	window.open("https://www.apple.com/iphone-13-pro/");
+}
+
+document.getElementById("pro13").addEventListener("click", thirteenpro);
+document.getElementById("thirteen").addEventListener("click", thirteen);
+document.getElementById("ise").addEventListener("click", se);
+document.getElementById("twelve").addEventListener("click", twelve);
+document.getElementById("eleven").addEventListener("click", eleven);
+document.getElementById("compare").addEventListener("click", compare);
+document.getElementById("ap").addEventListener("click", airpods);
+document.getElementById("at").addEventListener("click", airtag);
+document.getElementById("acc").addEventListener("click", accessories);
+document.getElementById("ac").addEventListener("click", applecard);
+document.getElementById("ios").addEventListener("click", ios);
+document.getElementById("shop").addEventListener("click", shop);
+document.getElementById("buy").addEventListener("click", buy);
+document.getElementById("link").addEventListener("click", link);
+document.getElementById("more").addEventListener("click", more);
