fc908e37a5be3a33151d90824d2cefe3403e49fe
MyaFryckman22 <817258@mystma.org>
2022-05-25 11:49:17 -0500

    I had to go back through to link a site I forgot, final project is not yet ready to be linked

---
+++ blackbear.js
@@ -0,0 +1,21 @@
+function habitat() {
+	document.open("habitat.html");
+}
+function eating() {
+	window.open("https://www.stma.k12.mn.us/Page/2#calendar1/20220422/month");
+}
+function behavior() {
+	window.open("https://www.stma.k12.mn.us/domain/4555");
+}
+function quiz() {
+	window.open("https://pvue7.region1.k12.mn.us/rt0885/PXP2_Login.aspx");
+}
+function about() {
+	window.open("https://stma.nutrislice.com/menus-eula");
+}
+
+document.getElementById("habitat").addEventListener("click", habitat);
+document.getElementById("eating").addEventListener("click", eating);
+document.getElementById("behavior").addEventListener("click", behavior);
+document.getElementById("quiz").addEventListener("click", quiz);
+document.getElementById("about").addEventListener("click", about);
